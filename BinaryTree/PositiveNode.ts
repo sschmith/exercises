bed6968199184eaff28e526231f56ef58edab565
Stephen Schmith <stephen.schmith@gmail.com>
2023-02-05 16:51:44 -0500

    exercise: throw exception if data less than -1

--- BinaryTree/PositiveNode.ts
+++ BinaryTree/PositiveNode.ts
@@ -2,6 +2,14 @@ import Node from "./Node";
 import type { Nullable } from "./types/Nullable";
 
 export default class PositiveNode extends Node {
+  constructor(data: number) {
+    if (data < -1) {
+      throw new Error(
+        "Illegal argument passed to PositiveNode: " + data.toString()
+      );
+    }
+    super(data);
+  }
   static create(data: Nullable<number>) {
     if (data == null || data < 0) return null;
     return new PositiveNode(data);
