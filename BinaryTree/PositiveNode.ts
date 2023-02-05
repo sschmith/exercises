b48f1ea61c2ecf53cc9dda43d50e21ae2cf5fe5b
Stephen Schmith <stephen.schmith@gmail.com>
2023-02-05 17:05:14 -0500

    exercise: prevent numbers less than -1

--- BinaryTree/PositiveNode.ts
+++ BinaryTree/PositiveNode.ts
@@ -2,14 +2,13 @@ import Node from "./Node";
 import type { Nullable } from "./types/Nullable";
 
 export default class PositiveNode extends Node {
+  static MIN_VALUE: number = -1;
+
   constructor(data: number) {
-    if (data < -1) {
-      throw new Error(
-        "Illegal argument passed to PositiveNode: " + data.toString()
-      );
-    }
-    super(data);
+    const positiveData = data >= -1 ? data : PositiveNode.MIN_VALUE;
+    super(positiveData);
   }
+
   static create(data: Nullable<number>) {
     if (data == null || data < 0) return null;
     return new PositiveNode(data);
